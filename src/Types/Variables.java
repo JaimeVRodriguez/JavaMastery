ce73212514250eb98782013f6c5870f2ebd3389c
Jaime Rodriguez <jaime@Jaimes-MacBook-Pro.local>
2022-08-20 09:52:31 -0400

    Add Variables Lesson

--- src/Types/Variables.java
+++ src/Types/Variables.java
@@ -3,6 +3,9 @@ package Types;
 public class Variables {
 
     public static void main(String[] args) {
-
+        int age = 30; //storing the number 30 to a variable called age
+        age = 35;
+        System.out.println(age);
+        
     }
 }
