c4a23425f6b5d3986ddaa947a7382a2ef4799825
Jaime Rodriguez <jaime@Jaimes-MacBook-Pro.local>
2022-08-21 10:01:02 -0400

    Add Arithmetic Lesson

--- src/Types/H_Constants.java
+++ src/Types/H_Constants.java
@@ -3,6 +3,6 @@ package Types;
 public class H_Constants {
     public static void main(String[] args) {
         final float PI = 3.14F; // final makes is so that variable can't be changed, also name should be all caps
-        
+
     }
 }
