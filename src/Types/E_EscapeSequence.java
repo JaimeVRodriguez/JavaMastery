e01d7b8b61d216f50c49ae038e39f759f39c40ff
Jaime Rodriguez <jaime@Jaimes-MacBook-Pro.local>
2022-08-21 09:14:22 -0400

    Add Escape Sequence Lesson

---
+++ src/Types/E_EscapeSequence.java
@@ -0,0 +1,11 @@
+package Types;
+
+public class E_EscapeSequence {
+    public static void main(String[] args) {
+        String message = "Hello \"Jaime\""; // the \ allows for the use of quotes
+        String messageTwo = "This will have \na new line"; // another escape sequence
+
+        System.out.println(message);
+        System.out.println(messageTwo);
+    }
+}
