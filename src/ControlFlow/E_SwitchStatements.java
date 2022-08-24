63a215deb7e9b07f1cd278656fa2d41e373fcdbf
Jaime Rodriguez <jaime@Jaimes-MacBook-Pro.local>
2022-08-24 18:54:37 -0400

    Add Switch Statement Lesson

---
+++ src/ControlFlow/E_SwitchStatements.java
@@ -0,0 +1,19 @@
+package ControlFlow;
+
+public class E_SwitchStatements {
+    public static void main(String[] args) {
+        String role = "moderator";
+
+        switch (role) {
+            case "admin":
+                System.out.println("You're an admin");
+                break;
+            case "moderator":
+                System.out.println("You're a moderator");
+                break;
+
+            default:
+                System.out.println("You're a guest");
+        }
+    }
+}
