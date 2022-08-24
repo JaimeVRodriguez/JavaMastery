50c61400e3001c948191eb51f73835344b905a00
Jaime Rodriguez <jaime@Jaimes-MacBook-Pro.local>
2022-08-24 18:48:46 -0400

    Add Ternary Operator Lesson

---
+++ src/ControlFlow/D_TernaryOperator.java
@@ -0,0 +1,9 @@
+package ControlFlow;
+
+public class D_TernaryOperator {
+    public static void main(String[] args) {
+        int income = 120_000;
+        String className = income > 100_000 ? "First" : "Economy";
+        // if the income is over 100,000 then the string will be defined as "First" if not then "Economy"
+    }
+}
