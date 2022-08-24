db480803acde43d4e10114d0d1b79892c9bcea3c
Jaime Rodriguez <jaime@Jaimes-MacBook-Pro.local>
2022-08-24 18:45:07 -0400

    Add If Statements Lesson

---
+++ src/ControlFlow/C_IfStatements.java
@@ -0,0 +1,24 @@
+package ControlFlow;
+
+public class C_IfStatements {
+    public static void main(String[] args) {
+        int temp = 32;
+
+        if (temp > 30) {
+            System.out.println("It's a hot day");
+            System.out.println("Drink water");
+        }
+        else if (temp > 20) {
+            System.out.println("Beautiful day");
+        }
+        else {
+            System.out.println("Cold day");
+        }
+
+
+        int income = 120_000;
+        boolean hasHighIncome = (income > 100_000);
+
+        System.out.println(hasHighIncome);
+    }
+}
