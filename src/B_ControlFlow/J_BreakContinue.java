401336e7145a8c9ab74bc36de8de38b659327d04
Jaime Rodriguez <jaimerod26@gmail.com>
2022-10-07 12:37:56 -0400

    Add Break/Continue Lesson

---
+++ src/B_ControlFlow/J_BreakContinue.java
@@ -0,0 +1,22 @@
+package B_ControlFlow;
+
+import java.util.Scanner;
+
+public class J_BreakContinue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+        while (!input.equals("quit")) {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            if (input.equals("pass")) {
+                continue;
+            }
+            if (input.equals("quit")) {
+                break;
+            }
+            System.out.println(input);
+        }
+    }
+}
