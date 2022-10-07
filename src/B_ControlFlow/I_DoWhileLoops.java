de1ac112349e162d2993544c55cc2f5236441ae7
Jaime Rodriguez <jaimerod26@gmail.com>
2022-10-07 12:32:05 -0400

    Add Do While Loops Lesson

---
+++ src/B_ControlFlow/I_DoWhileLoops.java
@@ -0,0 +1,21 @@
+package B_ControlFlow;
+
+import java.util.Scanner;
+
+public class I_DoWhileLoops {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+        do {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        }
+        while (!input.equals("quit"));
+
+
+    }
+}
